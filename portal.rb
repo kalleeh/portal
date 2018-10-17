5764c9a4b396c53b2a02b4be069ad97f7e90d719
kalleh <wallbomk@amazon.com>
2018-10-18 00:13:52 +0200

    cleaned up weather function

--- portal.rb
+++ portal.rb
@@ -12,12 +12,6 @@ get '/' do
   redirect '/index.html'
 end
 
-# The URI to do a temperature lookup for the specified city.
-# Returns a JSON document with city name, country and temperature.
-get '/weather/:city' do
-  lookup_weather(params['city'])
-end
-
 # The URI to do the stock symbol lookup.
 # Returns a JSON document with stock name and price.
 get '/stock/:symbol' do
@@ -27,15 +21,6 @@ end
 # The URI for the health check
 get '/health' do
   "OK"
-end
-
-# The function to lookup weather for a particular city.
-# Looks up the weather microservice and returns JSON doc
-def lookup_weather(city)
-  address, port = lookup_service('weather')
-  uri = URI.parse(URI.encode("http://#{address}:#{port}/weather/#{city}"))
-  res = Net::HTTP.get_response(uri)
-  res.body if res.is_a?(Net::HTTPSuccess)
 end
 
 # The function to lookup stock price for a particular symbol.
